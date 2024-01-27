1ea61868d74137d0acea8204a468a0492e6345f9
gabrielDouradoAtomIde <grddourado@gmail.com>
2024-01-26 22:30:08 -0300

    checkin a 100m da academia

--- src/services-use-cases/checkin/checkin.service.ts
+++ src/services-use-cases/checkin/checkin.service.ts
@@ -1,9 +1,14 @@
 import { Checkin } from "@prisma/client"
 import { CheckInsRepository } from "@/repositories/prisma/checkin-repository"
+import { GymsRepository } from "@/repositories/prisma/gyms-repository"
+import { InvalidResourceError } from "../errors/resource-not-found.error"
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates"
 
 interface CheckinServicieRequest {
     userId: string
     gymId: string
+    latitude: number
+    longitude: number
 }
 
 interface CheckinServiceResponse {
@@ -12,10 +17,25 @@ interface CheckinServiceResponse {
 
 // Service/Use Cases -> Lida com todas as alterações na percursão do dado ao banco
 export class CheckinService {
-    constructor(private checkinsRepository: CheckInsRepository) { }
+    constructor(
+        private checkinsRepository: CheckInsRepository,
+        private gymsRepository: GymsRepository
+    ) { }
 
-    executeCheckinService = async ({ userId, gymId }: CheckinServicieRequest): Promise<CheckinServiceResponse> => {
+    executeCheckinService = async ({ userId, gymId, latitude, longitude }: CheckinServicieRequest): Promise<CheckinServiceResponse> => {
+        const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
         const checkInOnSamedate = await this.checkinsRepository.findByUserIdOnDate(userId, new Date())
+        const gym = await this.gymsRepository.findById(gymId)
+
+        if (!gym) throw new InvalidResourceError()
+
+        const distante = getDistanceBetweenCoordinates(
+            { latitude, longitude },
+            { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+        )
+
+        if (distante > MAX_DISTANCE_IN_KILOMETERS) throw new Error()
 
         if (checkInOnSamedate) throw new Error()
 
--- src/services-use-cases/checkin/checkin.spec.ts
+++ src/services-use-cases/checkin/checkin.spec.ts
@@ -1,16 +1,29 @@
 import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest'
 import { InMemoryCheckinRepository } from '@/repositories/in-memory/in-memory-checkin.repository'
 import { CheckinService } from './checkin.service'
+import { InMemoryGymRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
 
 // it.skip || it.only
 let checkinRepository: InMemoryCheckinRepository
+let gymsRepository: InMemoryGymRepository
 let sut: CheckinService // S.U.T -> System Under Test
 
 describe('Testes para Checkin Services', () => {
     beforeEach(() => {
         checkinRepository = new InMemoryCheckinRepository()
-        sut = new CheckinService(checkinRepository)
+        gymsRepository = new InMemoryGymRepository()
+        sut = new CheckinService(checkinRepository, gymsRepository)
         vi.useFakeTimers()
+
+        gymsRepository.items.push({
+            id: 'gym-01',
+            title: 'Academia JS',
+            description: '',
+            latitude: new Decimal(0),
+            longitude: new Decimal(0),
+            phone: ''
+        })
     })
 
     afterEach(() => {
@@ -21,7 +34,9 @@ describe('Testes para Checkin Services', () => {
 
         const { checkIn } = await sut.executeCheckinService({
             gymId: 'gym-01',
-            userId: '123-321'
+            userId: '123-321',
+            latitude: 0,
+            longitude: 0
         })
 
         expect(checkIn.id).toEqual(expect.any(String))
@@ -31,13 +46,17 @@ describe('Testes para Checkin Services', () => {
         vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
         await sut.executeCheckinService({
             gymId: 'gym-01',
-            userId: '123-321'
+            userId: '123-321',
+            latitude: 0,
+            longitude: 0
         })
 
         expect(async () => {
             await sut.executeCheckinService({
                 gymId: 'gym-01',
-                userId: '123-321'
+                userId: '123-321',
+                latitude: 0,
+                longitude: 0
             })
         }).rejects.toBeInstanceOf(Error)
     })
@@ -46,16 +65,43 @@ describe('Testes para Checkin Services', () => {
         vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
         await sut.executeCheckinService({
             gymId: 'gym-01',
-            userId: '123-321'
+            userId: '123-321',
+            latitude: 0,
+            longitude: 0
         })
 
         vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
         const { checkIn } = await sut.executeCheckinService({
             gymId: 'gym-01',
-            userId: '123-321'
+            userId: '123-321',
+            latitude: 0,
+            longitude: 0
         })
 
         expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it('Não deve ser possível checar-se distante da academia', async () => {
+        //16.0596005,-47.9611932
+
+        gymsRepository.items.push({
+            id: 'gym-02',
+            title: 'Academia JS',
+            description: '',
+            latitude: new Decimal(16.0596005),
+            longitude: new Decimal(47.9611932),
+            phone: ''
+        })
+
+        expect(async () => {
+            await sut.executeCheckinService({
+                gymId: 'gym-02',
+                userId: '123-321',
+                latitude: -16.0428182,
+                longitude: -47.861419
+            })
+        }).rejects.toBeInstanceOf(Error)
+
     })
 
 })
